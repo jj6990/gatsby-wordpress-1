d762a52d502bedecddbd8fb6ab6cabab6f4a37a9
Juan Jose Terneus <juanjose.terneus@verndale.com>
2022-03-09 22:42:41 -0500

    adding real cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `http://localhost:8888/gatsby-wordpress/graphql`,
+          `https://cms.develup.tech/graphql`,
       },
     },
 
